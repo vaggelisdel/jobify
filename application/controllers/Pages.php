b008f5f3852edd5d4472eb144e553dd88efc3f89
Dimitrios Ganotis <dimitrisgan97@gmail.com>
2019-02-20 20:54:30 +0200

    added Pages Controller for simple pages like homepage

---
+++ application/controllers/Pages.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Κλάση για απλές σελίδες όπως η αρχική
+class Pages extends CI_Controller {
+  protected $metadata = array(
+    'title' => null,
+    'description' => 'Το Jobify είναι μια online πλατφόρμα αγγελιών εργασίας'
+  );
+
+  // Αρχική Σελίδα
+  public function index() {
+    // 404 σφάλμα όταν το view δεν υπάρχει
+    if(!file_exists('application/views/guest_user_area/pages/homepage.php'))
+      show_404();
+    
+    $this->metadata['title'] = 'Jobify';
+
+    $this->load->view('guest_user_area/pages/homepage', $this->metadata);
+  }
+}
---
+++ application/views/guest_user_area/pages/homepage.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php echo $title; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <h1 style="text-align: center; margin-top: 10rem;">Καλωσήρθες στο Jobify!</h1>
+</body>
+</html>
