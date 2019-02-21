4c0cf7e9d9f5cc022a3d6f6b74aa4abe9bf7ead7
Dimitrios Ganotis <dimitrisgan97@gmail.com>
2019-02-21 19:32:35 +0200

    renames

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -10,10 +10,10 @@ class Pages extends CI_Controller {
   // Αρχική Σελίδα
   public function index() {
     // 404 σφάλμα όταν το view δεν υπάρχει
-    if(!file_exists('application/views/guest_user_area/pages/homepage.php'))
+    if(!file_exists('application/views/public_area/pages/homepage.php'))
       show_404();
-    
-    $this->metadata['title'] = 'Jobify';
+
+      $this->metadata['title'] = 'Jobify';
 
     $this->load->view('public_area/pages/homepage', $this->metadata);
   }
